df489bcc94ee7a51634c8d9e2e6a91e8bbd26986
Freddy L <neo-ciber_94@hotmail.com>
2025-05-14 20:02:52 -0400

    Limit media size

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,6 +1,9 @@
 "use server";
 import mime from "mime";
 
+const DEFAULT_MAX_SIZE_MB = 100; // 100mb
+const MAX_MEDIA_SIZE_MB = process.env.MAX_MEDIA_SIZE_MB;
+
 export async function searchMedia(url: string) {
   const res = await fetch(url, {
     method: "HEAD",
@@ -22,6 +25,23 @@ export async function searchMedia(url: string) {
     return {
       error: "Unsupported media type only audio, video and image are supported",
     };
+  }
+
+  const responseBodySizeInBytes = getResponseSizeInBytes(res.headers);
+
+  if (MAX_MEDIA_SIZE_MB !== "ignore") {
+    if (responseBodySizeInBytes == null) {
+      return {
+        error: "Failed to get the media size",
+      };
+    }
+
+    if (!isMediaSizeValid(responseBodySizeInBytes)) {
+      const maxSize = MAX_MEDIA_SIZE_MB || DEFAULT_MAX_SIZE_MB;
+      return {
+        error: `Media size is too large. Max size is ${maxSize}mb`,
+      };
+    }
   }
 
   const pendingData = fetchBlobData(url);
@@ -83,4 +103,44 @@ function getMediaTypeFromContentType(contentType: string) {
   }
 
   return null;
+}
+
+function getResponseSizeInBytes(headers: Headers) {
+  const contentLength = headers.get("content-length");
+
+  if (!contentLength) {
+    return null;
+  }
+
+  const sizeBytes = parseInt(contentLength, 10);
+
+  if (isNaN(sizeBytes)) {
+    return null;
+  }
+
+  return sizeBytes;
+}
+
+function isMediaSizeValid(sizeBytes: number) {
+  const mediaMaxSizeMb = getMaxSizeMb();
+
+  if (!mediaMaxSizeMb) {
+    return true;
+  }
+
+  const maxSizeBytes = mediaMaxSizeMb * 1024 * 1024;
+  return sizeBytes <= maxSizeBytes;
+}
+
+function getMaxSizeMb() {
+  if (MAX_MEDIA_SIZE_MB === "ignore") {
+    return null;
+  }
+
+  if (!MAX_MEDIA_SIZE_MB) {
+    return DEFAULT_MAX_SIZE_MB;
+  }
+
+  const mediaMaxSizeMb = parseInt(MAX_MEDIA_SIZE_MB, 10);
+  return mediaMaxSizeMb;
 }
